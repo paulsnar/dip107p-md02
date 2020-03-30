a06aa6139d16a7207f03bb189d03ba40dd956b1d
paulsnar <paulsnar@paulsnar.lv>
2020-03-30 15:13:38 +0300

    v1.0.1

--- src/main/java/lv/paulsnar/edu/dip107p/md02/Main.java
+++ src/main/java/lv/paulsnar/edu/dip107p/md02/Main.java
@@ -11,7 +11,7 @@ public final class Main {
   private Main() {
   }
 
-  public static final String VERSION = "1.0.0";
+  public static final String VERSION = "1.0.1";
 
   private static int main() {
     try (Database db = new Database(new File("database.dat"))) {
