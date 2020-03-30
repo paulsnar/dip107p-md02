86a9c207bbfe50f45217a55cd4f84436de2755ad
paulsnar <paulsnar@paulsnar.lv>
2020-03-30 13:53:43 +0300

    v1.0.0

--- src/main/java/lv/paulsnar/edu/dip107p/md02/Main.java
+++ src/main/java/lv/paulsnar/edu/dip107p/md02/Main.java
@@ -8,7 +8,7 @@ import lv.paulsnar.edu.dip107p.md02.ui.UserDialogue;
 public final class Main {
   private Main() { }
 
-  public static final String VERSION = "0.1.0";
+  public static final String VERSION = "1.0.0";
 
   private static int main() {
     try (Database db = new Database(new File("database.dat"))) {
